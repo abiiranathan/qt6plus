a2838fd62f04a85e9618480bcdd4d6a0bf3eb4c8
Dr. Abiira Nathan <nabiira2by2@gmail.com>
2023-06-11 18:35:56 +0300

    add shapeDrawing functions

--- GraphicsScene.h
+++ GraphicsScene.h
@@ -19,9 +19,9 @@
 class GraphicsScene : public QGraphicsScene {
  public:
   GraphicsScene(QObject* parent = nullptr) : QGraphicsScene(parent) {
-    setSceneRect(QRectF(-500, -500, 1000, 1000));
     currentPen = QPen(Qt::black);
     currentBrush = QBrush(Qt::transparent);
+
     isDrawing = false;
     currentPathItem = nullptr;
   }
@@ -99,13 +99,31 @@ class GraphicsScene : public QGraphicsScene {
   }
 
   void wheelEvent(QGraphicsSceneWheelEvent* event) override {
-    // Zoom in or out
-    int zoomFactor = event->delta() > 0 ? 2 : -2;
-    QList<QGraphicsView*> sceneViews = views();
-    if (!sceneViews.isEmpty()) {
-      QGraphicsView* view = sceneViews.at(0);
-      view->scale(zoomFactor, zoomFactor);
+    if (event->modifiers() == Qt::ControlModifier) {
+      // Zoom in or out
+      int zoomFactor = event->delta() > 0 ? 2 : -2;
+
+      // Get the cursor position in the scene coordinates
+      QPointF cursorPos = event->scenePos();
+
+      // Get the associated view
+      QGraphicsView* view = views().at(0);
+      if (view) {
+        qreal scaleFactor = qPow(2, zoomFactor / 100.0);
+        view->scale(scaleFactor, scaleFactor);
+
+        // Calculate the new cursor position after scaling
+        QPointF newCursorPos = cursorPos * scaleFactor;
+
+        // Adjust the view's scroll position to keep the cursor at the same
+        // position
+        QPointF scrollOffset = newCursorPos - event->scenePos();
+        setSceneRect(sceneRect().translated(-scrollOffset));
+      }
     }
+
+    // Call the base class implementation to handle other wheel events
+    QGraphicsScene::wheelEvent(event);
   }
 
   void keyPressEvent(QKeyEvent* event) override {
@@ -205,6 +223,170 @@ class GraphicsScene : public QGraphicsScene {
     QPainter painter(&image);
     render(&painter);
     image.save(filePath);
+  }
+
+  void drawTriangle(const QPointF& position, qreal size,
+                    const QPen& pen = QPen(Qt::black),
+                    const QBrush& brush = Qt::NoBrush) {
+    // Create a QGraphicsPolygonItem and add it to the scene
+    QGraphicsPolygonItem* triangleItem = new QGraphicsPolygonItem();
+    triangleItem->setPos(position);
+
+    // Define the triangle's points
+    QPolygonF trianglePolygon;
+    trianglePolygon << QPointF(0, 0) << QPointF(size, 0)
+                    << QPointF(size / 2, size);
+
+    // Set the triangle's polygon and properties
+    triangleItem->setPolygon(trianglePolygon);
+    triangleItem->setPen(pen);
+    triangleItem->setBrush(brush);
+
+    addItem(triangleItem);  // Add the triangle item to the scene
+  }
+
+  void drawTriangle(const QPointF& point1, const QPointF& point2,
+                    const QPointF& point3, const QPen& pen = QPen(Qt::black),
+                    const QBrush& brush = Qt::NoBrush) {
+    // Create a QPainterPath and move to the first point
+    QPainterPath path;
+    path.moveTo(point1);
+
+    // Draw lines to connect the three points
+    path.lineTo(point2);
+    path.lineTo(point3);
+    path.lineTo(point1);
+
+    // Create a QGraphicsPathItem and set the path
+    QGraphicsPathItem* pathItem = new QGraphicsPathItem();
+    pathItem->setPath(path);
+
+    pathItem->setBrush(brush);
+    pathItem->setPen(pen);
+
+    // Add the path item to the scene
+    addItem(pathItem);
+  }
+
+  void createSquare(const QPointF& position, qreal size,
+                    const QPen& pen = QPen(Qt::black),
+                    const QBrush& brush = Qt::NoBrush) {
+    // Create a QGraphicsRectItem and add it to the scene
+    QGraphicsRectItem* squareItem = new QGraphicsRectItem();
+    squareItem->setPos(position);
+
+    // Set the square's geometry and properties
+    squareItem->setRect(0, 0, size, size);
+
+    squareItem->setBrush(brush);
+    squareItem->setPen(pen);
+
+    addItem(squareItem);  // Add the square item to the scene
+  }
+
+  void createRectangle(const QPointF& position, qreal width, qreal height,
+                       const QPen& pen = QPen(Qt::black),
+                       const QBrush& brush = Qt::NoBrush) {
+    // Create a QGraphicsRectItem and add it to the scene
+    QGraphicsRectItem* rectangleItem = new QGraphicsRectItem();
+    rectangleItem->setPos(position);
+
+    // Set the rectangle's geometry and properties
+    rectangleItem->setRect(0, 0, width, height);
+    rectangleItem->setBrush(brush);
+    rectangleItem->setPen(pen);
+
+    addItem(rectangleItem);  // Add the rectangle item to the scene
+  }
+
+  void createCircle(const QPointF& position, qreal size,
+                    const QPen& pen = QPen(Qt::black),
+                    const QBrush& brush = Qt::NoBrush) {
+    QGraphicsEllipseItem* circleItem = new QGraphicsEllipseItem();
+    circleItem->setPos(position);
+
+    // Set the circle's geometry and properties
+    circleItem->setRect(0, 0, size, size);
+    circleItem->setBrush(brush);
+    circleItem->setPen(pen);
+
+    addItem(circleItem);  // Add the circle item to the scene
+  }
+
+  void createSphere(const QPointF& position, const qreal size,
+                    const QPen& pen = QPen(Qt::black),
+                    const QBrush& brush = Qt::NoBrush) {
+    // Create a QGraphicsEllipseItem and add it to the scene
+    QGraphicsEllipseItem* sphereItem = new QGraphicsEllipseItem();
+    sphereItem->setPos(position);
+
+    // Set the sphere's geometry and properties
+    sphereItem->setRect(0, 0, size, size);
+    sphereItem->setBrush(brush);
+    sphereItem->setPen(pen);
+    addItem(sphereItem);
+  }
+
+  void drawQuadraticCurve(const QPointF& startPoint,
+                          const QPointF& controlPoint, const QPointF& endPoint,
+                          const QPen& pen = QPen(Qt::black),
+                          const QBrush& brush = Qt::NoBrush) {
+    // Create a QPainterPath and move to the start point
+    QPainterPath path;
+    path.moveTo(startPoint);
+
+    // Draw the smooth curve
+    path.quadTo(controlPoint, endPoint);
+
+    // Create a QGraphicsPathItem and set the path
+    QGraphicsPathItem* pathItem = new QGraphicsPathItem();
+    pathItem->setPath(path);
+
+    // Set the pen and brush properties for the curve
+    pathItem->setBrush(brush);
+    pathItem->setPen(pen);
+
+    // Add the path item to the scene
+    addItem(pathItem);
+  }
+
+  void drawBezierCurve(const QPointF& startPoint, const QPointF& endPoint,
+                       const QPointF& controlPoint1,
+                       const QPointF& controlPoint2,
+                       const QPen& pen = QPen(Qt::black),
+                       const QBrush& brush = Qt::NoBrush) {
+    // Create a QPainterPath and move to the start point
+    QPainterPath path;
+    path.moveTo(startPoint);
+
+    // Draw the Bézier curve
+    path.cubicTo(controlPoint1, controlPoint2, endPoint);
+
+    // Create a QGraphicsPathItem and set the path
+    QGraphicsPathItem* pathItem = new QGraphicsPathItem();
+    pathItem->setPath(path);
+    pathItem->setBrush(brush);
+    pathItem->setPen(pen);
+
+    // Add the path item to the scene
+    addItem(pathItem);
+  }
+
+  void addText(const QPointF& startPoint, const QString& text,
+               const QFont& font = QFont()) {
+    QGraphicsTextItem* textItem = new QGraphicsTextItem(text);
+    textItem->setFont(font);
+    textItem->setPos(startPoint);
+    addItem(textItem);
+  }
+
+  void addHTMLText(const QPointF& startPoint, const QString& html,
+                   const QFont& font = QFont()) {
+    QGraphicsTextItem* textItem = new QGraphicsTextItem();
+    textItem->setHtml(html);
+    textItem->setFont(font);
+    textItem->setPos(startPoint);
+    addItem(textItem);
   }
 
  private:
--- main.cpp
+++ main.cpp
@@ -22,7 +22,7 @@ int main(int argc, char *argv[]) {
 
   // Create the custom splitter
   Splitter splitter;
-  splitter.setOrientation(Qt::Horizontal);
+  splitter.setOrientation(Qt::Vertical);
 
   // Add widgets to the splitter
   splitter.addWidget(label1, 1);
@@ -30,8 +30,7 @@ int main(int argc, char *argv[]) {
   splitter.addWidget(label2, 3);
 
   // Set the stretch factors for the splitter widgets
-  //  splitter.setWidgetStretchFactors(1, 2, 3);
-  splitter.setWidgetSizes(200, 600, 200);
+  //  splitter.setWidgetSizes(200, 600, 200);
 
   // Add the splitter to the layout
   layout.addWidget(&splitter);
@@ -43,23 +42,49 @@ int main(int argc, char *argv[]) {
   // Test Graphics scene
   QGraphicsView view;
   GraphicsScene scene;
+  scene.setSceneRect(0, 0, 400, 400);
+
+  QPen pen(Qt::red, 2, Qt::SolidLine, Qt::FlatCap, Qt::BevelJoin);
+  scene.setPen(pen);
 
   view.setScene(&scene);
+  // Enable anti-aliasing for the QGraphicsView
+  view.setRenderHint(QPainter::Antialiasing);
+
   view.setWindowTitle("GraphicsScene Test");
 
   // Set up the initial pen color and brush color
   scene.setBrushColor(Qt::transparent);
   scene.setBackgroundBrush(Qt::white);
 
-  QPen pen(Qt::red, 2, Qt::SolidLine, Qt::FlatCap, Qt::BevelJoin);
-  scene.setPen(pen);
+  scene.createCircle(QPointF(10, 10), 100, pen, Qt::blue);
+
+  scene.addLine(QLineF({5, 5}, {200, 100}));
+
+  scene.drawBezierCurve(QPointF(100, 100), QPointF(300, 100), QPointF(150, 50),
+                        QPointF(250, 150));
+
+  scene.drawQuadraticCurve(QPointF(100, 100), QPointF(300, 100),
+                           QPointF(200, 0));
+
+  scene.createRectangle(QPointF(100, 200), 200, 60, QPen(Qt::cyan),
+                        QBrush(Qt::DiagCrossPattern));
+
+  scene.addText(QPoint(10, 200), "HELLO ABIIRA NATHAN",
+                QFont("Arial", 18, 600));
+
+  scene.drawTriangle(QPointF(20, 50), QPointF(200, 75), QPointF(300, 300),
+                     QPen(Qt::cyan), QBrush(Qt::blue));
+
   //  scene.setBrushStyle(Qt::DiagCrossPattern);  // Set brush style
   //  scene.saveToFile("image.png");  // Save the scene as an image file
 
   // Show the view
-  view.show();
+  //  view.show();
 
-  //  mainWindow.show();
+  splitter.addWidget(&view);
+  splitter.setWidgetStretchFactors(1, 2, 3);
+  mainWindow.show();
 
   return app.exec();
 }
